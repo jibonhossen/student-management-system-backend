4b47a23db45efcfe50cedcef24b98dc0cafb2054
Jibon Hossen <jibonhossen@gmail.com>
2025-11-14 21:52:05 +0600

    change some code in Heath-Check controllers

--- controllers/healthcheck.controllers.js
+++ controllers/healthcheck.controllers.js
@@ -3,11 +3,7 @@ import ApiResponse from "../utils/api-respons.js";
 import ApiError from "../utils/api-error.js";
 
 const healthCheckController = asyncHandler(async (req, res) => {
-  let count = 0;
-  console.log("Health check controller",count);
-  count++;
   res.status(200).json(new ApiResponse(200,{message: "Server is running!"}));
-
 });
 
 export default healthCheckController;
